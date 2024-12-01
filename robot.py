9b241c713ff40430f9b8b85f07ce616c9448d18c
PandaBlitz994 <pandablitz.994@gmail.com>
2024-12-01 20:30:25 +0200

    Declares ports, setss colors and proto start mechanisem

--- robot.py
+++ robot.py
@@ -5,38 +5,32 @@ from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
 from pybricks.tools import hub_menu
 
-
+# Declaring ports
 hub = PrimeHub()
-left_wheel = Motor(Port.F, Direction.COUNTERCLOCKWISE) #Red
-right_wheel = Motor(Port.B) #Cyan
-left_arm = Motor(Port.D) #Yellow
-right_arm = Motor(Port.A) #Purple
-chassis = DriveBase(left_wheel, right_wheel, 62.4, 81) #black
-front_color = ColorSensor(Port.C) #Green
-back_color = ColorSensor(Port.E)  #Blue
+left_wheel = Motor(Port.A, Direction.COUNTERCLOCKWISE) #Red
+right_wheel = Motor(Port.E) #Cyan
+left_arm = Motor(Port.F) #Yellow
+right_arm = Motor(Port.B) #Purple
+chassis = DriveBase(left_wheel, right_wheel, 62.4, 81)
+front_color = ColorSensor(Port.D) #Green
+back_color = ColorSensor(Port.C)  #Blue
 chassis.use_gyro(True) 
 DriveBase.settings(chassis, 200)
 
-black = Color(180,9,24)
-red = Color(352,90,96)
-yellow = Color(90,29,100)
-blue = Color(213,92,85)
-green = Color(154,78,65)
+# Setting detectebale colors for back light sensor
+Color.BLACK = Color(180,9,24)
+Color.RED = Color(352,90,96)
+Color.YELLOW = Color(90,29,100)
+Color.BLUE = Color(213,92,85)
+Color.GREEN = Color(154,78,65)
+ccolors = (Color.BLACK, Color.RED, Color.YELLOW, Color.BLUE, Color.GREEN)
+back_color.detectable_colors(ccolors)
 
-back_color.detectable_colors(
-    [
-        black, red, yellow, blue, green
-    ]
-)
-
-# print(back_color.color(True))
-chassis.straight(1000)
-# print(front_color.hsv())
-# left_arm.run_time(300,5000)
-# for port in [Port.A, Port.B... ]:
-#     try: 
-#         m = Motor(port)
-#         m.run()
-#         break
-#     except OSError:
-#         print("incorrect port")
+color = back_color.color()
+if color == Color.BLACK:
+    print("hello")
+elif color == Color.RED:
+    print("hello")
+elif color == Color.YELLOW:
+    
+elif color == Color.BLUE:
